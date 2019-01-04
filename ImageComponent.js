19c2cfbd982a32c3082e2cbbb3d68ebc0f973bb5
dingzuoqiang <530858106@qq.com>
2019-01-04 19:53:03 +0800

    初识 react native组件

---
+++ ImageComponent.js
@@ -0,0 +1,18 @@
+/**
+ * Created by DingZuoQiang on 2019/1/4.
+ */
+
+import React, {Component} from 'react';
+import {View,Image} from 'react-native';
+
+export default class ImageComponent extends Component {
+  render() {
+      let pic = {
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+      };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}} />
+    );
+  }
+}
+
