250aa1fcb171fe9a657b5ae31c5cb3d4f7cdc714
dingzuoqiang <530858106@qq.com>
2019-01-04 20:29:38 +0800

    滚动视图：ScrollView

--- App.js
+++ App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import ImageComponent from './ImageComponent';
+import {Platform, StyleSheet, Text, View,ScrollView} from 'react-native';
+import ImageComponent from './study_js/ImageComponent';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,13 +21,21 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+        <ScrollView>
+        <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <ImageComponent />
-      </View>
-    );
+        <View style={{marginBottom: 10}}>
+          <ImageComponent />
+        </View>
+        <View style={styles.marginBottom_s}>
+          <ImageComponent />
+        </View>
+
+        </View>
+        </ScrollView>
+  );
   }
 }
 
@@ -46,6 +54,9 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
+    marginBottom: 5,
+  },
+  marginBottom_s: {
     marginBottom: 5,
   },
 });
